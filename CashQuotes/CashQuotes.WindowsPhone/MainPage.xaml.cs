53200f05befa96f64e83099afa2f77e46b225198
Boris <atronax@gmail.com>
2015-03-12 19:59:23 +0300

    add page to display quotes

--- CashQuotes/CashQuotes.WindowsPhone/MainPage.xaml.cs
+++ CashQuotes/CashQuotes.WindowsPhone/MainPage.xaml.cs
@@ -1,48 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CashQuotes
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public static MainPage Current;
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-        }
+		public MainPage()
+		{
+			this.InitializeComponent();
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // TODO: Prepare page for display here.
+			this.NavigationCacheMode = NavigationCacheMode.Required;
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-        }
-    }
+			// This is a static public property that allows downstream pages to get a handle to the MainPage instance
+			// in order to call methods that are in this class.
+			Current = this;
+
+			Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.
+		/// This parameter is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			if (MainFrame.Content == null)
+			{
+				if (!MainFrame.Navigate(typeof(QuoteListPage)))
+				{
+					throw new Exception("Failed to navigate to a page with a list of Quotes");
+				}
+			}
+		}
+
+		void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+		{
+			if (MainFrame.CanGoBack)
+			{
+				MainFrame.GoBack();
+
+				//Indicate the back button press is handled so the app does not exit
+				e.Handled = true;
+			}
+		}
+	}
 }
---
+++ CashQuotes/CashQuotes.WindowsPhone/QuoteListPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace CashQuotes
+{
+	/// <summary>
+	/// Page that lists exchange rates (quotes) in a gridview
+	/// </summary>
+	public sealed partial class QuoteListPage : Page
+	{
+		public QuoteListPage()
+		{
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// Invoked when this page is about to be displayed in a Frame.
+		/// </summary>
+		/// <param name="e">Event data that describes how this page was reached.
+		/// This parameter is typically used to configure the page.</param>
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			// TODO: ItemGridView.ItemsSource = data from service
+		}
+
+		private void RefreshAppBarButton_Click(object sender, RoutedEventArgs e)
+		{
+			// TODO: ItemGridView.ItemsSource = data from service
+		}
+
+		private void SortAppBarButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			// TODO: show a menu to choose how to sort the grid data
+		}
+
+	}
+}
